809873270cce8d142b25ca93972367f592b85860
Rhys Evans <rhskaevans@gmail.com>
2024-09-20 14:33:45 -0600

    made calc

--- DEmooooooo210/Program.cs
+++ DEmooooooo210/Program.cs
@@ -7,6 +7,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//This was added to give access to components we will work with
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
 //
